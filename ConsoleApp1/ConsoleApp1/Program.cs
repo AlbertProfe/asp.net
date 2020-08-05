ce1bca6ff3c5c01920565ca0a1b1015da454b71a
AlbertProfe <albertprofe@gmail.com>
2020-08-05 10:51:12 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If exemple....");
+
+            int x = 20;
+            int y = 18;
+
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+        }
+    }
+}
