d937d57dd13e42c5aa2e960cf78adbba8ceb4cc2
AlbertProfe <albertprofe@gmail.com>
2020-08-05 10:58:49 +0200

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!!!!!");
+            Console.WriteLine("Hello DevOps");
+            Console.WriteLine("Hello Repo");
+
+
+            Console.WriteLine("New Line");
+
+            Console.WriteLine("Hello VS2019 from repo DevOps from Azure!!!!");
+
+            IfStatement(50);
+        }
+
+        static void IfStatement(int value)
+        {
+            if (value == 0)
+            {
+                Console.WriteLine("No number");
+            }
+            else
+            {
+                Console.WriteLine("There is a number");
+            }
+        }
+
+        static void IfStatement2(int value)
+        {
+            if (value == 0)
+            {
+                Console.WriteLine("No number");
+            }
+            else
+            {
+                Console.WriteLine("There is a number");
+            }
+        }
+
+
+    }
+}
